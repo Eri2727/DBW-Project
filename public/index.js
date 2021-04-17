a82d1930debeed6d877d40c6d8336677ac12f57d
m3ida <75899733+m3ida@users.noreply.github.com>
2021-04-17 20:31:48 +0000

    login, register, etc

---
+++ public/index.js
@@ -0,0 +1,30 @@
+
+
+
+function checkFile(input) {
+    var file = input.files[0];
+    var fileType = file["type"];
+    var validImageTypes = ["image/jpeg", "image/png"];
+
+    if ($.inArray(fileType, validImageTypes) < 0) {
+        alert("Invalid File. Please attach a valid .png or .jpeg file.");
+        location.reload();
+    }
+
+    if (input.files && input.files[0] ) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+
+            $('#imageAdded').attr('src', e.target.result);
+            $('#imageAdded').removeAttr('hidden');
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#inputPicture").change(function(){
+    checkFile(this);
+
+});
